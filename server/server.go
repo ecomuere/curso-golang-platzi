0c51b762eaa4db7dcd2353ed2ab6d254b3971373
ivan <ivan@whoomi.com>
2016-08-11 23:25:49 +0200

    server básico

---
+++ server/server.go
@@ -0,0 +1,20 @@
+/**
+ * Webserver
+ */
+
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+
+func main () {
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "URL.Path = %q\n", r.URL.Path)
+}
